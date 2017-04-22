0f134b90c3ee15a29cabfe6f024b01ff1bcaf94f
id_rsa_phabricator <yifang.ma@midea.com>
2017-04-22 16:06:21 +0800

    初始化项目

---
+++ MYFDemo/MYFDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MYFDemo
+//
+//  Created by MaYifang on 2017/4/22.
+//  Copyright © 2017年 MYF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MYFDemo/MYFDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MYFDemo
+//
+//  Created by MaYifang on 2017/4/22.
+//  Copyright © 2017年 MYF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
